343be0fd68ffa15f9eaa98b9f29ed10112ea6263
takum <takuma.ishikawa@ad-dice.biz>
2026-01-21 20:21:13 +0900

    テストシステム構築

---
+++ abc214/a/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+
+    if (n <= 125) {
+        cout << 4 << endl;
+    } else if (n <= 211) {
+        cout << 6 << endl;
+    } else {
+        cout << 8 << endl;
+    }
+
+    return 0;
+}
