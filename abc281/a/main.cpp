1b2ea7a63f4eb13cd4f486878d3b36cc636a4cf0
takum <takuma.ishikawa@ad-dice.biz>
2026-01-28 21:44:31 +0900

    main編集

--- abc281/a/main.cpp
+++ abc281/a/main.cpp
@@ -8,7 +8,11 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    
+    int n;
+    cin >> n;
+    for (int i = n; i >= 0; i--) {
+        cout << i << "\n";
+    }
     
     return 0;
 }
