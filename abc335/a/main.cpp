c9c7fb03581d7b68bdd6a79ade75f720f1ea5ac3
takum <takuma.ishikawa@ad-dice.biz>
2026-02-11 15:08:42 +0900

    そる部

---
+++ abc335/a/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string S;
+    cin >> S;
+    cout << S.substr(0, S.length()-1) << 4 << endl;
+    
+    return 0;
+}
---
+++ abc340/a/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+using ll = long long;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a,b,d;
+    cin >> a >> b >> d;
+    bool first = true;
+    for (int x = a; x <= b; x += d){
+        if (!first) cout << ' ';
+        cout << x;
+        first = false;
+    }
+    cout << '\n';
+    return 0;
+}
