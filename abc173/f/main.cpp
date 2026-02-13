6240153344200c28ad9bc08e908c8a38902fc39c
takum <takuma.ishikawa@ad-dice.biz>
2026-02-13 16:09:31 +0900

    ソルブ

--- abc173/f/main.cpp
+++ abc173/f/main.cpp
@@ -8,11 +8,7 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int a, b, c;
-    cin >> a >> b >> c;
-    vector<int> v = {a, b, c};
-    sort(v.begin(), v.end());
-    cout << v[2] << v[1] << v[0] << "\n";
+    // write code here
     
     return 0;
 }
