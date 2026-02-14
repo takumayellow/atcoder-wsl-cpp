23baf3d96a44bd86fd21a5f2ce1ce55f6540c08a
takum <takuma.ishikawa@ad-dice.biz>
2026-02-14 18:35:50 +0900

    テンプレート調整 rep

--- abc168/a/main.cpp
+++ abc168/a/main.cpp
@@ -8,12 +8,15 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    int n;
-    cin >> n;
-    if(n%1000==0){
-        cout << 0 << "\n";
+    int N;
+    cin >> N;
+
+    if (N % 10 == 3) {
+        cout << "bon" << endl;
+    } else if (N % 10 == 0 || N % 10 == 1 || N % 10 == 6 || N % 10 == 8){
+        cout << "pon" << endl;
     } else{
-        cout << 1000 - n % 1000 << "\n";
+        cout << "hon" << endl;
     }
     return 0;
 }
--- tools/templates/cpp/main.cpp
+++ tools/templates/cpp/main.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 using namespace atcoder;
 using ll = long long;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
+
+
     return 0;
 }
